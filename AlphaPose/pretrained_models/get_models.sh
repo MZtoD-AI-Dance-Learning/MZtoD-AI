f3716944b60839ae5d7c1cc72b133de6e34d5467
changduckGO <62056652+changduckGO@users.noreply.github.com>
2022-06-04 18:37:14 +0900

    Update: get models added

---
+++ AlphaPose/pretrained_models/get_models.sh
@@ -0,0 +1,3 @@
+# Field = 1SS2jUWzhqnU6lhJnruDxLHrPVBT_9gVA
+# Name
+wget --load-cookies ~/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SS2jUWzhqnU6lhJnruDxLHrPVBT_9gVA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1SS2jUWzhqnU6lhJnruDxLHrPVBT_9gVA" -O fast_res50_256x192.pth && rm -rf ~/cookies.txt
